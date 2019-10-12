3158feaf8f17248f504e06cbe063927660a50e51
zhengyc <1010172623@qq.com>
2019-10-12 20:56:11 +0800

    update

--- threeCarsOne/js/path.js
+++ threeCarsOne/js/path.js
@@ -1,6 +1,6 @@
-//$server_path = "http://148.70.40.47:8080/app";
-$server_path = "http://192.168.2.102:8080/app";
-$wxId = "1212odHLq5jSebhOFKllB6xelPSo6Szk";
+$server_path = "http://148.70.40.47:8080/app";
+//$server_path = "http://192.168.1.7:8080/app";
+$wxId = "";
 $pageSize=5;
 
 Date.prototype.format = function(format)
@@ -72,7 +72,21 @@ function getStatus1(name){
 		return '<span style="color:green">订单完成</span>';
 	}else if(name =='50'){
 		return  '<span style="color:green">已结算</span>';
+	}else if(name =='60'){
+		return  '<span style="color:green">车夫取消</span>';
 	}else{
 		return "";
 	}	
+}
+
+
+function getQueryVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
 }
